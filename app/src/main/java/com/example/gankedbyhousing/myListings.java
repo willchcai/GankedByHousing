278f062ca423eacc0aa66ac4eb21c88f08de1e91
WillPC <willchcai@gmail.com>
2020-06-08 01:51:38 -0700

    added navbar & make listing skeleton code onto master code

--- app/src/main/java/com/example/gankedbyhousing/myListings.java
+++ app/src/main/java/com/example/gankedbyhousing/myListings.java
@@ -1,6 +1,5 @@
 package com.example.gankedbyhousing;
 
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -11,24 +10,18 @@ import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-
-
-
-public class MainActivity extends AppCompatActivity {
-
+public class myListings extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-
+        setContentView(R.layout.activity_my_listings);
 
         BottomNavigationView navbar;
 
         navbar = findViewById(R.id.navbar);
         Menu menu = navbar.getMenu();
-        MenuItem menuItem = menu.getItem(0);
+        MenuItem menuItem = menu.getItem(1);
         menuItem.setChecked(true);
 
         navbar.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -36,22 +29,22 @@ public class MainActivity extends AppCompatActivity {
             public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
                 switch (menuItem.getItemId()){
                     case R.id.nav_profile:
-                        Intent toProfile = new Intent(MainActivity.this, ProfileActivity.class);
+                        Intent toProfile = new Intent(myListings.this, ProfileActivity.class);
                         startActivity(toProfile);
                         break;
                     case R.id.my_listings:
-                        Intent myListings = new Intent(MainActivity.this, myListings.class);
-                        startActivity(myListings);
                         break;
                     case R.id.listings:
+                        Intent toMainActivity = new Intent(myListings.this, MainActivity.class);
+                        startActivity(toMainActivity);
                         break;
                     case R.id.view_listings:
-                        Intent toViewListings = new Intent(MainActivity.this, ViewListingsActivity.class);
+                        Intent toViewListings = new Intent(myListings.this, ViewListingsActivity.class);
                         startActivity(toViewListings);
                         break;
                     case R.id.make_listings:
-                        Intent toUpload = new Intent(MainActivity.this, MakeListings.class);
-                        startActivity(toUpload);
+                        Intent toMakeListings = new Intent(myListings.this, MakeListings.class);
+                        startActivity(toMakeListings);
                         break;
 
                 }
@@ -59,7 +52,5 @@ public class MainActivity extends AppCompatActivity {
                 return true;
             }
         });
-
-
     }
 }
